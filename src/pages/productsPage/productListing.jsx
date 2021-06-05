0f01c3ba28805ca46c9793e20b79b96a361214c3
saisumanthkacham <63229560+saisumanthkacham@users.noreply.github.com>
2021-06-05 15:30:55 +0530

    style: margin updated

--- src/pages/productsPage/productListing.jsx
+++ src/pages/productsPage/productListing.jsx
@@ -40,8 +40,8 @@ return <section className="body">
         <div onClick={()=>dispatch({type:"CLEAR-ALL-FILTERS"})}className="btn secondary-bg ">
          Clear Filters
         </div>
-        
-        <ul> {/*this ul is created just to reflect border on ui*/}</ul> 
+        <br/>
+        <hr/>
 
         <ul>
             <b>PLATFORMS</b><br/>
@@ -49,32 +49,32 @@ return <section className="body">
               <li><input name="platform-sort" type="radio" checked={state.filters.instagram}  onChange={()=>{dispatch({type:"CLEAR-ALL-FILTERS"});dispatch({type:"INSTAGRAM"})}} /><label>Instagram</label></li>
               <li><input name="platform-sort" type="radio"  checked={state.filters.linkedin} onChange={()=>{dispatch({type:"CLEAR-ALL-FILTERS"});dispatch({type:"LINKEDIN"})}} /><label>Linkedin</label></li>
               <li><input name="platform-sort" type="radio" checked={state.filters.twitter}  onChange={()=>{dispatch({type:"CLEAR-ALL-FILTERS"});dispatch({type:"TWITTER"})}} /><label>Twitter</label></li>
-              <br/>
         </ul>
-           
+              <hr/>
         <ul>
             <b>PRICE</b><br/>
               <li><input type="radio" name="sort"  onClick={()=>dispatch({type:"HIGH-TO-LOW"})}/>
               <label>High to low</label></li>
               <li><input type="radio" name="sort"  onClick={()=>dispatch({type:"LOW-TO-HIGH"})}/>
               <label>Low to high</label></li>
-              <br/>
         </ul>
-             
+              <hr/>
         <ul>
             <b>SORT</b><br/>
             <li><input type="checkbox" checked={state.filters.outOfStock} onChange={()=>dispatch({type:"INCLUDE-OUT-OF-STOCK"})}/><label>Include out of stock</label></li> 
             <li><input type="checkbox" checked={state.filters.fastDelivery} onChange={()=>dispatch({type:"ONLY-FAST-DELIVERY"})}/><label>FastDelivery</label></li>
-            <br/>
+            
         </ul>
+              <hr/>
+
         <br/><br/><br/>👇<br/>
         <b>FUN FEATURE:</b><br/>  
         <div className="black-bg">
         <p className="primary-font">Wanna have some Fun, you can try editing this website by just clicking the below button </p>
         <div className=" btn grey-font" onClick={()=>document.designMode="on"}> Edit Website</div>
         <div className=" btn white-font" onClick={()=>document.designMode="off"}> stop editing</div><br/>
-
         </div>
+        
     </section>
 
 
